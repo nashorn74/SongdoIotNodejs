13cb51e5ef8b4f9f413e8dae7ace0866a6fa2cf0
Jeong kum ho <nashorn74@gmail.com>
2017-11-01 16:19:17 +0900

    first commit

---
+++ ExpressExam/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'HelloWorld' });
+  res.send(JSON.stringify({method:'GET', url:'/'}));
+});
+
+router.post('/', function(req, res, next) {
+	res.send(JSON.stringify({method:'POST', url:'/'}));
+});
+
+router.put('/', function(req, res, next) {
+	res.send(JSON.stringify({method:'PUT', url:'/'}));
+});
+
+router.delete('/', function(req, res, next) {
+	res.send(JSON.stringify({method:'DELETE', url:'/'}));
+});
+
+module.exports = router;
